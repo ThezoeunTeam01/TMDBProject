26420df915d6d115b62ceb013af8800af9c62ecb
ByeungJi <dprmakak@gmail.com>
2023-12-19 14:59:45 +0900

    test

---
+++ src/main/java/com/example/tmdbproject/TmdbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmdbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tmdbproject/TmdbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmdbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
